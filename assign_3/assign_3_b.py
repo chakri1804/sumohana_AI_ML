4e86400ce7272760881e3364326f8f79c0992e7d
chakri1804 <chakri1804@gmail.com>
2018-09-07 13:58:09 +0530

    SVM modified

--- assign_3/assign_3_b.py
+++ assign_3/assign_3_b.py
@@ -1,102 +1,106 @@
+
+# coding: utf-8
+
+# In[3]:
+
+
 import numpy as np
+import cvxpy as cp
+import pandas as pd
 
-# Parameters
-print("Give Number of Hidden Layer nodes :")
-M = int(input())
-print("Give number of training samples per bit pair:")
-n = int(input())
-print("Give std.dev for noise :")
-noise = float(input())
-print("Give number of Epochs :")
-epochs = int(input())
-print("Give learning rate :")
-lr = float(input())
-print("XOR/OR/AND? eg.Enter XOR for XOR")
-option = input()
+df_X = pd.read_csv('Xsvm.csv',header=None)
+df_Y = pd.read_csv('ysvm.csv',header=None)
+X = np.array(df_X,dtype=np.float64)
+Y = np.array(df_Y,dtype=np.float64)
+print(X.shape,Y.shape)
 
-# Generate Datasets
 
-Input = np.array([[0,0],[0,1],[1,0],[1,1]])
-if(option == "OR"):
-    Output = np.array([[0],[1],[1],[1]])
-if(option == "AND"):
-    Output = np.array([[0],[0],[0],[1]])
-if(option == "XOR"):
-    Output = np.array([[0],[1],[1],[0]])
+# In[4]:
 
-X = []
-Y = []
 
-for i in range(len(Input)):
-	for j in range(n):
-		X.append(Input[i]+np.random.normal(0,noise,(1,2)))
-		Y.append(Output[i]+np.random.normal(0,noise))
+# Convex Optimization
+a = cp.Variable(len(Y))
+R1 = cp.matmul(cp.diag(a),Y)
+R2 = cp.matmul(X.T,R1)
+R4 = cp.norm(R2)**2
+R4.shape
 
-X = np.array(X)
-X = X.reshape((len(Y),2))
-Y = np.array(Y)
 
-# defining functions
+# In[5]:
 
-def sigm(x):
-	return 1/(1+np.exp(-x))
 
-def diff_sigm(x):
-	return (sigm(x)-sigm(x)**2)
+P1 = cp.sum(a)
+Const1 = P1 - 0.5*R4
+# Const1 = np.reshape(Const1,(1,))
 
-def layer(x,W,b):
-	return (np.matmul(W.T,x.reshape(len(x),1)) + b)
 
-def sq_err(y,Y):
-	return (y-Y)**2
-# Initializing weights
+# In[6]:
 
-W1 = np.random.normal(0,1,(2,M))
-Bi1 = np.random.normal(0,1,(M,1))
-W2 = np.random.normal(0,1,(M,1))
-Bi2 = np.random.normal(0,1,(1,1))
 
-# training
+Const2 = cp.matmul(a.T,Y)
+Const3 = [0<=a,Const2 == 0]
+obj = cp.Maximize(Const1)
+prob = cp.Problem(obj, Const3)
+prob.solve(verbose=True)
 
-for i in range(epochs):
-	w1 = np.zeros(W1.shape)
-	b1 = np.zeros(Bi1.shape)
-	w2 = np.zeros(W2.shape)
-	b2 = np.zeros(Bi2.shape)
-	for j in range(len(Y)):
-		#forward path
-		out1 = layer(X[j],W1,Bi1)
-		# print(out1)
-		z = sigm(out1)
-		# print(z)
-		out2 = layer(z,W2,Bi2)
-		y = sigm(out2)
-		# print(y)
-		#backpropagation
-		b2 += 2*(y-Y[j])*diff_sigm(out2)
-		w2 += 2*(y-Y[j])*diff_sigm(out2)*z
-		loss = sq_err(y,Y[j])
-		for k in range(M):
-			b1[k] += (2*(y-Y[j])*diff_sigm(out2)*diff_sigm(out1[k])*W2[k]).reshape(1,)
-			w1[:,k] += (2*(y-Y[j])*diff_sigm(out2)*diff_sigm(out1[k])*W2[k]*X[j]).reshape(2,)
-	print(loss)
-	W1 -= lr*w1
-	W2 -= lr*w2
-	Bi1 -= lr*b1
-	Bi2 -= lr*b2
 
-while(1):
-    a=input("Enter test sample: ").split(',')
-    for i in range(0,2):
-        a[i]=float(a[i])
-    a=np.array(a)
-    print(a.shape)
-    out_1 = layer(a,W1,Bi1)
-    z = sigm(out_1)
-    out_2 = layer(z,W2,Bi2)
-    y_pred= sigm(out_2)
-    if y_pred > 0.5:
-        y_pred = 1
-    else :
-        y_pred = 0
-    print(y_pred)
+# In[7]:
+
+
+print(a.value)
+
+
+# In[8]:
+
+
+A = (np.array(a.value)).reshape(500,1)
+
+
+# In[9]:
+
+
+W = np.zeros((2,))
+for i in range(len(Y)):
+    W += A[i]*Y[i]*(X[i].T)
+    if(A[i]>1e-4):
+        print(i)
+
+
+# In[10]:
+
+
+print(W)
+
+
+# In[11]:
+
+
+W0 = (1/Y[281]) - np.dot(W,X[281])
+print(W0)
+
+
+# In[12]:
+
+
+Test = np.array([[2,0.5],[0.8,0.7],[1.58,1.33],[0.008, 0.001]])
+
+for i in range(len(Test)):
+    est = np.sign(np.dot(W,Test[i])+W0)
+    print(Test[i],est)
+
+
+# In[14]:
+
+
+# Verification
+from sklearn import svm
+
+clf = svm.SVC()
+clf.fit(X,Y)
+
+
+# In[15]:
+
+
+clf.predict(Test)
+
