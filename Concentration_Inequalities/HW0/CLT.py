d29ec6719462b2d8d75b820a46e4a999700c57e9
chakri1804 <chakri1804@gmail.com>
2019-01-20 17:20:56 +0530

    Basics

---
+++ Concentration_Inequalities/HW0/CLT.py
@@ -0,0 +1,42 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+np.random.seed(42)
+
+# Determine the number of IIDs taken for CLT approximation
+n = 10000
+# Number of samples being called for each IIDs
+k = 100000
+# Uniform Random Variable
+a = np.zeros((k,))
+for i in range(n):
+    a += np.random.uniform(-100,100,(k,))
+a = a/n
+
+# Rayleigh RV with scale 1
+b = np.zeros((k,))
+for i in range(n):
+    b += np.random.rayleigh(1, (k,))
+b = b/n
+
+# Poisson RV with Lambda = 1
+c = np.zeros((k,))
+for i in range(n):
+    c += np.random.poisson(1,(k,))
+c = c/n
+
+# Log-Normal RV
+d = np.zeros((k,))
+for i in range(n):
+    d += np.random.lognormal(0,0.1,(k,))
+d = d/n
+
+clts = [a,b,c,d]
+
+fig, ax = plt.subplots(4, 1)
+
+for i in range(len(clts)):
+    hist, bins = np.histogram(clts[i], bins=50, normed=True)
+    bin_centers = (bins[1:]+bins[:-1])*0.5
+    ax[i].plot(bin_centers, hist)
+plt.show()
---
+++ Concentration_Inequalities/HW0/LLN.py
@@ -0,0 +1,47 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+## LLN basically states as we take more and more samples from a single distribution and take the mean of them
+## The mean tends to the mean of the original distribution
+
+## Here I am plotting a graph of the mean of the samples drawn from a given distribution vs
+## the number of samples chosen
+
+# Uniform Random Variable
+n1 = 10000
+a = []
+for i in range(n1):
+    a.append(np.mean(np.random.uniform(-10,10,(i,))))
+
+# Log-Normal Random Variable
+n2 = 10000
+b = []
+mean = 0.0
+sigma = 1.0
+for i in range(n2):
+    b.append(np.mean(np.random.lognormal(mean,sigma,(i,))))
+
+act_mean = np.exp(mean + (sigma**2)/2)
+# Gaussian Random Variable
+n3 = 10000
+c = []
+for i in range(n3):
+    c.append(np.mean(np.random.normal(0,1,(i,))))
+
+# Laplace Random Variable
+n4 = 10000
+d = []
+for i in range(n4):
+    d.append(np.mean(np.random.laplace(0,0.1,(i,))))
+
+llns = [a,b,c,d]
+
+fig, ax = plt.subplots(4, 1)
+
+for i in range(len(llns)):
+    ax[i].plot(llns[i])
+    if (i != 1):
+        ax[i].plot(np.zeros(np.shape(llns[i])), 'r')
+    else:
+        ax[i].plot(np.zeros(np.shape(llns[i])) + act_mean, 'r')
+plt.show()
